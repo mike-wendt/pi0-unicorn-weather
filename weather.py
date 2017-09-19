51703cdc11100db0a4f2f3c6d71ed61e435b0048
Mike Wendt <mike.wendt@me.com>
2017-09-19 16:15:19 -0400

    comment cleanup

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@
 import unicornhat as uh
 import time
 
-# colors used by the app
+# Colors used by the app [r,g,b]
 CLEAR_COLOR=[255,191,0]
 RAIN_COLOR=[0,72,186]
 SNOW_COLOR=[0,255,255]
@@ -12,7 +12,7 @@ FOG_COLOR=[255,0,255]
 CLOUDY_COLOR=[240,248,255]
 PARTLY_CLOUDY_COLOR=[90,94,97]
 
-# names used by darksky.net https://darksky.net/dev/docs#data-point-object
+# Names used by darksky.net https://darksky.net/dev/docs#data-point-object
 ICON_NAMES=['clear-day', 'clear-night', 'rain', 'snow', 'sleet', 'wind', 'fog', 'cloudy', 'partly-cloudy-day', 'partly-cloudy-night']
 ICON_COLORS=[CLEAR_COLOR,CLEAR_COLOR,RAIN_COLOR,SNOW_COLOR,SNOW_COLOR,WIND_COLOR,FOG_COLOR,CLOUDY_COLOR,PARTLY_CLOUDY_COLOR,PARTLY_CLOUDY_COLOR]
 ICON_DEFAULT_COLOR=[63,0,0]
@@ -22,6 +22,7 @@ uh.set_layout(uh.PHAT)
 uh.rotation(0)
 uh.brightness(0.6)
 
+# Helpers to set pixel colors
 def set_color(x,y,color):
   uh.set_pixel(x,y,color[0],color[1],color[2])
 
